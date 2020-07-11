6224750d33260fc1fbfa0b6b547ac77d17a2b604
Angelo Domingo <jellodomingo@gmail.com>
2020-07-11 11:16:55 -0700

    Changed animation, deleted projects.html

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function showHome() {
-    document.getElementById("projects-page").style.display = 'none';
-    document.getElementById("home-page").style.opacity = 0;
-    document.getElementById("home-page").style.display = 'block';
-
+function enterWebsiteAnimate() {
     let tl = anime.timeline({
         easing: 'easeOutExpo',
         duration: 100
@@ -10,56 +6,74 @@ function showHome() {
     
     tl.add({
         targets: '#home-page',
-        translateY: -150
+        translateX: 400
+    });
+    
+    tl.add({
+        targets: '#topbar',
+        translateY: -100
+    });
+    
+    tl.add({
+        targets: '#topbar, #home-page',
+        translateX: 0,
+        translateY: 0,
+        opacity: 1,
+        duration: 1200
+    });
+}
+
+function enterHomeAnimate() {
+    let tl = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: 100
     });
     
     tl.add({
         targets: '#home-page',
-        translateY: 0,
-        opacity: 1,
-        duration: 1000
+        translateX: 400
     });
+    
+    tl.add({
+        targets: '#home-page',
+        translateX: 0,
+        opacity: 1,
+        duration: 1200
+    });
+}
+
+function enterProjectsAnimate(){
+    let tl = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: 100
+    });
+    
+    tl.add({
+        targets: '#projects-page',
+        translateX: 400
+    });
+    
+    tl.add({
+        targets: '#projects-page',
+        translateX: 0,
+        opacity: 1,
+        duration: 1200
+    });
+}
+
+function showHome() {
+    document.getElementById("projects-page").style.display = 'none';
+    document.getElementById("home-page").style.opacity = 0;
+    document.getElementById("home-page").style.display = 'block';
+    enterHomeAnimate();
 }
 
 function showProjects() {
     document.getElementById("home-page").style.display = 'none';
     document.getElementById("projects-page").style.opacity = 0;
     document.getElementById("projects-page").style.display = 'block';
-
-    let tl = anime.timeline({
-        easing: 'easeOutExpo',
-        duration: 100
-    });
-    
-    tl.add({
-        targets: '#projects-page',
-        translateY: -150
-    });
-    
-    tl.add({
-        targets: '#projects-page',
-        translateY: 0,
-        opacity: 1,
-        duration: 1000
-    });
+    enterProjectsAnimate();
 }
 
-let tl = anime.timeline({
-    easing: 'easeOutExpo',
-    duration: 100
-});
-
-tl.add({
-    targets: '#home-page',
-    translateY: -150
-});
-
-tl.add({
-    targets: '#home-page',
-    translateY: 0,
-    opacity: 1,
-    duration: 1000
-});
-
 //START UP ANIMATION
-
+enterWebsiteAnimate();
